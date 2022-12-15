9c6693f31ad9470b657c64125734a03959d4612c
Hoàng Phúc <lehoangphuc1859@gmail.com>
2022-12-15 12:12:48 +0700

    Bootstrap

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const hbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//http logger
+app.use(morgan("combined"));
+
+//template engine
+app.engine(
+  "hbs",
+  hbs.engine({
+    // đuôi file
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/danh-muc", (req, res) => {
+  res.render("category");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
